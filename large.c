4efd3b8515de28cffeffd23828497089d2a28f4c
karthigaiselvi <35292744+karthigaiselvi@users.noreply.github.com>
2018-02-14 21:21:35 +0530

    Create large.c

---
+++ large.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+if(a>b)
+{
+if(a>c)
+printf("a is big");
+else
+printf("c is big");
+}
+else
+{
+if(b>c)
+printf("b is big");
+else
+printf("c is big");
+}
+}
